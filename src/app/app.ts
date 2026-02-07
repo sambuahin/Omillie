44b4f73fbdd39ac8c2ba4d941b5837e53787df08
Samuel Buahin <sambuahin@gmail.com>
2026-02-07 06:50:43 -0500

    adding sections

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,14 @@ import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FrontBanner } from "./front-banner/front-banner";
 import { Navigation } from "./navigation/navigation";
+import { OrderOfService } from "./order-of-service/order-of-service";
+import { Tributes } from "./tributes/tributes";
+import { Hymns } from "./hymns/hymns";
+import { Gallery } from "./gallery/gallery";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Navigation, FrontBanner],
+  imports: [RouterOutlet, Navigation, FrontBanner, OrderOfService, Tributes, Hymns, Gallery],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
